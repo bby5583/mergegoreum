34207c5a6d914695a58a539f48e2ce83f6941301
bby5583 <147736210+bby5583@users.noreply.github.com>
2023-10-13 00:16:57 +0900

    Update settings.js

--- src/settings.js
+++ src/settings.js
@@ -1,5 +1,5 @@
 window._CCSettings = {
-  platform: "web-mobile",
+  platform: "web",
   groupList: ["default", "bg", "ui", "wall", "fruit", "line", "downwall", ""],
   collisionMatrix: [
     [false, null, null, false],
